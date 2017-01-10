78cfdf0c54576eed3be1d57bd3bef11743c0f818
blong4 <blong4@studentmail.nait,ca>
2017-01-09 18:57:48 -0700

    Added Connetion Strings Etc

--- ChinookSystem/DAL/ChinookContext.cs
+++ ChinookSystem/DAL/ChinookContext.cs
@@ -4,10 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespaces
+using System.Data.Entity;
+using ChinookData.Entities;
+#endregion
+
 namespace ChinookSystem.DAL
 {
-    internal class ChinookContext
+    internal class ChinookContext:DbContext
     {
+        public ChinookContext():base("ChinookDB")
+        {
+            
+        }
 
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
     }
 }
